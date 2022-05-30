376da2718debbac62314a371df3a030bb6b78b56
vlad_mir <mironow.slava2012@yandex.ru>
2022-05-30 19:39:25 +0400

    Сделал первую задачутест сработал

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,16 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  "use strict"
+  d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    arr = [];
+  }
+  else if (d == 0) {
+    arr = [-b / (2 * a)];
+  }
+  else {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
   return arr; // array
 }
 
