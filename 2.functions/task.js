9ec5038eee1d4b834ea0ab7aa5acb5fd6d301f55
vlad_mir <mironow.slava2012@yandex.ru>
2022-07-02 15:34:50 +0400

    Пытался сделать второе задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -10,26 +10,40 @@ function getArrayParams(arr) {
 // Задание 2
 function worker(arr) {
   let sum = 0;
+  let max = arr[0];
+  let min = arr[0];
+  let avg;
 
+for (let i of arr) {
+  if (digit < min) {
+    min = digit;
+  }
+  if (digit > max) {
+    max = digit;
+  }
 
+  sum += digit;
+}
+
+avg = sum / arr.length;
 
   return sum;
 }
 
+worker([1, 2, 3]);
+worker([4, 5, 6]);
+let arrOfArr = [[1, 2, 3], [4, 5, 6]];
 function makeWork(arrOfArr, func) {
   let max = arrOfArr[0];
-
-
-
-  for (func(arr) of arrOfArr) {
-    if (func(arr) > max) {
-      max = sum;
+  for (func of arrOfArr) {
+    if (func > max) {
+      max = func;
   } 
 }
   
   return max;
 }
-
+ makeWork(arrOfArr, worker)
 
 
 
