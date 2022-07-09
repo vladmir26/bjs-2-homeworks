bb26abaf19516453645f0e07f1e39c706973ef77
vlad_mir <mironow.slava2012@yandex.ru>
2022-07-09 19:06:13 +0400

    Пытался сделать первое задание с массивами

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,22 @@
 function compareArrays(arr1, arr2) {
   let result;
 
+  if (arr1.every((element, index) => element === arr2[index])) {
+      result = true;
+  } else {
+    result = false;
+  }
+  
+
+  
   // Ваш код
 
   return result; // boolean
 }
+
+
+
+
 
 function advancedFilter(arr) {
   let resultArr;
