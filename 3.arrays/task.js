6b90e97c09f02ad0468585da3fef8d159e577bf4
vlad_mir <mironow.slava2012@yandex.ru>
2022-07-15 17:56:06 +0400

    Сделал вторую задачу с массивами

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -14,9 +14,10 @@ function compareArrays(arr1, arr2) {
 
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter((element) => element > 0).filter((element) => element % 3 === 0).map((element) => element * 10);
 
   // Ваш код
 
   return resultArr; // array
 }
+
