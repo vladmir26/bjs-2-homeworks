79a384a9a4ec3d2d697629f06acd0d19cb4c580e
vlad_mir <mironow.slava2012@yandex.ru>
2022-07-22 12:18:37 +0400

    Сделанное задание с прототипами

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+      this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+    if (this.marks === undefined) {
+      this.marks = marks;
+    } else {
+      const combinedArray = this.marks.concat(marks);
+      this.marks = combinedArray;
+    }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+
+  for (let i of this.marks) {
+    sum += i;
+  }
+  let avg = sum / this.marks.length;
+
+  return avg;
+}
+
+Student.prototype.exclude = function (reason) { 
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
+}
+
+const student = new Student("Alex", "m", 28);
