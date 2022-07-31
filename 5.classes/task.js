2f8a211c63782f157f4c9583e1d9e5b880a2098e
vlad_mir <mironow.slava2012@yandex.ru>
2022-07-31 15:14:39 +0400

    Пытался сделать первую задачу

---
+++ 5.classes/task.js
@@ -0,0 +1,14 @@
+class PrintEditionItem {
+    constructor (name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix() {
+      this.state * 1,5;
+    }
+  
+}
