eee912ee2f94671bf387ab89ac666adbbf47f194
vlad_mir <mironow.slava2012@yandex.ru>
2022-08-10 21:06:23 +0400

    Пытался сделать задачу с классами

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -8,7 +8,58 @@ class PrintEditionItem {
     }
 
     fix() {
-      this.state * 1,5;
+      this.state = this.state * 1,5;
+    } 
+
+    set state(newState) {
+      if (newState < 0) {
+        this._state = 0;
+      } else if (newState > 100) {
+        this._state = 100;
+      } else {
+        this._state = newState;
+      }
+    } 
+
+    get state () {
+      return this._state;
     }
-  
+
+    }
+
+class Magazine extends PrintEditionItem {
+      constructor (name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
+      }
+    }
+
+class Book extends PrintEditionItem {
+      constructor (name, releaseDate, pagesCount, author) {
+        super(name, releaseDate, pagesCount, author);
+        this.type = "book";
+        this.author = author;
+        
+    }
+  }
+
+class NovelBook extends Book {
+    constructor (name, releaseDate, pagesCount, author) {
+      super(name, releaseDate, pagesCount, author);
+      this.type = "novel";
+  }
+}
+
+class FantasticBook extends Book {
+  constructor (name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "fantactic";
+  }
+}
+
+class DetectiveBook extends Book {
+  constructor (name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "detective";
+  }
 }
