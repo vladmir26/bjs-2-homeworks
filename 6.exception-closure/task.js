7293ef3f721d3b09150733f34a8be2a3166564ee
vlad_mir <mironow.slava2012@yandex.ru>
2022-10-24 16:18:21 +0400

    Сделанное первое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -2,10 +2,27 @@ function parseCount(a) {
    
     let parse = Number.parseInt(a); 
 
-       if (Number.parseInt(a) === NaN) {
+    if (Object.is(parse,NaN)) {
          let error = new Error("Невалидное значение");
          throw error;
     }
+
  return parse;
 }
 
+
+function validateCount(b) {
+
+try {
+     
+     let parse = parseCount(b);
+
+if(parse) {
+     return parse;
+   }
+}
+catch (e) {
+  
+     return e;
+}
+}
